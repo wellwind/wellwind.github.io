6f7327da77b54a44d95f4a3414473e14ac2e7690
Mike Huang <wellwind@mail2000.com.tw>
2021-10-22 00:50:32 +0800

    display toc

--- src/app/blog/blog-post-toc/blog-post-toc.component.ts
+++ src/app/blog/blog-post-toc/blog-post-toc.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { AfterViewInit, Component, Inject, Input, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-blog-post-toc',
@@ -9,14 +10,27 @@ export class BlogPostTocComponent implements OnInit, AfterViewInit {
   @Input() rootElement!: HTMLElement;
   @Input() contentElement!: HTMLElement
 
-  constructor() {
+  headings: { text: string, level: number, element: HTMLElement }[] = [];
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
   }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit() {
-    console.log(this.rootElement, this.contentElement);
+    if (isPlatformServer(this.platformId)) {
+      return;
+    }
+    let result: any[] = [];
+    this.contentElement.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(head => {
+      result = [...result, {
+        text: head.textContent,
+        level: +head.tagName.replace(/h(.)/i, '$1'),
+        element: head
+      }]
+    });
+    this.headings = result;
   }
 
 }
