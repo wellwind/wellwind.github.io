71fd48dc5f02409b1ffb36dc257763368f79ab5f
Mike Huang <wellwind@mail2000.com.tw>
2021-10-28 23:30:09 +0800

    build rss

--- builders/src/generate-rss/index.ts
+++ builders/src/generate-rss/index.ts
@@ -1,9 +1,8 @@
 import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
 import { JsonObject } from '@angular-devkit/core';
 import { readdirSync, writeFileSync } from 'fs';
-import { range } from 'ramda';
 import { getMarkdownMeta } from '../../../utils/get-markdown-meta';
-import { slugify } from '../../../utils/slugify';
+import { Feed } from 'feed';
 
 interface Options extends JsonObject {
   markdownPostsPath: string;
@@ -16,8 +15,6 @@ async function generateRss(options: Options, context: BuilderContext): Promise<B
   const markdownPostsPath = options.markdownPostsPath;
   const rssPath = options.rssPath;
 
-  const rss
-
   context.logger.info(`Generate ${rssPath}.`);
 
   const posts = readdirSync(markdownPostsPath, { withFileTypes: true })
@@ -25,8 +22,36 @@ async function generateRss(options: Options, context: BuilderContext): Promise<B
     .map(dirent => dirent.name)
     .map(fileName => getMarkdownMeta(markdownPostsPath, fileName));
 
-  writeFileSync(rssPath, urls.join('\n'));
+  const siteUrl = 'https://fullstackladder.dev/';
+  const feed = new Feed({
+    title: '全端開發人員天梯',
+    description: '軟體開發學不完，就像爬不完的天梯，只好多紀錄寫筆記',
+    id: siteUrl,
+    link: siteUrl,
+    language: 'zh',
+    favicon: `${siteUrl}/favicon.ico`,
+    copyright: 'All rights reserved 2021, Mike Huang',
+    author: {
+      name: 'Mike Huang',
+      email: 'wellwind@gmail.com',
+      link: siteUrl
+    }
+  });
 
-  context.logger.info('Done');
+  posts.forEach(post => {
+    const dateFormatted = new Date(post.date).toISOString().slice(0, 10).replace(/-/g, '/');
+
+    feed.addItem({
+      title: post.title,
+      id: `${siteUrl}${dateFormatted}/${post.slug}`,
+      link: `${siteUrl}${dateFormatted}/${post.slug}`,
+      description: post.summary,
+      date: new Date(post.date),
+    });
+  })
+
+  writeFileSync(rssPath, feed.rss2());
+
+  context.logger.info('✅ Done');
   return { success: true };
 }
