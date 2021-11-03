fbae7814aa92d6f773567218655859737bf12c7d
Mike Huang <wellwind@mail2000.com.tw>
2021-11-03 11:48:14 +0800

    avoid toc flickering

--- src/app/blog/blog-posts/blog-post-toc/blog-post-toc.component.ts
+++ src/app/blog/blog-posts/blog-post-toc/blog-post-toc.component.ts
@@ -1,7 +1,19 @@
 import { ContentObserver } from '@angular/cdk/observers';
 import { AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { makeStateKey, TransferState } from '@angular/platform-browser';
+import { Observable, ReplaySubject } from 'rxjs';
+import { map, startWith, switchMap, tap } from 'rxjs/operators';
 import { PlatformService } from '../../../../platform.service';
 import { findMainContentContainer, scrollTo } from '../../../../utils';
+
+interface Heading {
+  text: string,
+  level: number,
+  element?: HTMLElement,
+  active: boolean
+}
+
+const HEADINGS_CACHE_KEY = makeStateKey('POST_TOC');
 
 @Component({
   selector: 'app-blog-post-toc',
@@ -9,87 +21,147 @@ import { findMainContentContainer, scrollTo } from '../../../../utils';
   styleUrls: ['./blog-post-toc.component.scss']
 })
 export class BlogPostTocComponent implements OnInit, AfterViewInit, OnDestroy {
-  @Input() contentElement!: HTMLElement
+  private _contentElement!: HTMLElement;
+  private _contentElement$ = new ReplaySubject<HTMLElement>(1);
 
-  headings: { text: string, level: number, element: HTMLElement, active: boolean }[] = [];
-  intersectionObserver?: IntersectionObserver | null;
+  @Input()
+  set contentElement(value: HTMLElement) {
+    this._contentElement = value;
+    this._contentElement$.next(this._contentElement)
+  }
 
-  constructor(private platformService: PlatformService, private contentObserver: ContentObserver) {
+  headings$ = this._contentElement$.pipe(
+    switchMap(contentElement =>
+      this.contentObserver.observe(contentElement)
+        .pipe(
+          map(() => contentElement),
+          startWith(contentElement)
+        )
+    ),
+    map(element => ({ cacheHeadings: this.getCacheHeadings(), element })),
+    map(data => {
+      if (data.cacheHeadings && data.cacheHeadings.length > 0) {
+        if (!this.platformService.isServer) {
+          this.removeCacheHeadings()
+        }
+        return ({ headings: data.cacheHeadings, element: data.element });
+      } else {
+        const tocHeadings = this.getTocHeadings(data.element);
+        if (this.platformService.isServer) {
+          this.setCacheHeadings(tocHeadings);
+        }
+        return ({ headings: tocHeadings, element: data.element });
+      }
+    }),
+    map(data => {
+      // client 端重新抓一次 toc
+      if (!this.platformService.isServer) {
+        return { headings: this.getTocHeadings(data.element), element: data.element };
+      }
+      return data;
+    }),
+    switchMap((data) => this.getContentTocHeadings(data.headings, data.element)),
+    startWith(this.getCacheHeadings())
+  );
+
+  private _intersectionObserver?: IntersectionObserver | null;
+
+  constructor(
+    private transferState: TransferState,
+    private platformService: PlatformService,
+    private contentObserver: ContentObserver) {
   }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit() {
-    if (this.platformService.isServer) {
-      return;
-    }
-
-    setTimeout(() => {
-      this.contentObserver.observe(this.contentElement).subscribe(() => {
-        if (this.intersectionObserver) {
-          this.intersectionObserver.disconnect();
-          this.intersectionObserver = null;
-        }
-        this.setTocScrollSpy();
-      });
-      this.setTocScrollSpy();
-    });
   }
 
-  private setTocScrollSpy() {
+  getCacheHeadings() {
+    return this.transferState.get<Heading[]>(HEADINGS_CACHE_KEY, []);
+  }
+
+  setCacheHeadings(headings: Heading[]) {
+    this.transferState.set<Heading[]>(HEADINGS_CACHE_KEY, headings);
+  }
+
+  removeCacheHeadings() {
+    this.transferState.remove(HEADINGS_CACHE_KEY)
+  }
+
+  getContentTocHeadings(headings: Heading[], element: HTMLElement) {
+    return new Observable<Heading[]>(subscriber => {
+      // 先回傳目前內容
+      subscriber.next(headings);
+
+      if (this.platformService.isServer) {
+        subscriber.complete();
+        return;
+      }
+
+      // client 計算要 active 的 element
+
+      const containerElement = element.closest('.mat-drawer-content.main-content');
+      let options = {
+        root: containerElement,
+        rootMargin: '0px',
+        threshold: 1.0
+      }
+
+      let visibleElements: HTMLElement[] = [];
+      if (this._intersectionObserver) {
+        this._intersectionObserver.disconnect();
+        this._intersectionObserver = null;
+      }
+
+      this._intersectionObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            visibleElements = [...visibleElements, entry.target as HTMLElement];
+          } else {
+            visibleElements = visibleElements.filter(element => element !== entry.target);
+          }
+        });
+
+        if (visibleElements.length > 0) {
+          const visibleElement = visibleElements.sort((a, b) => a.offsetTop - b.offsetTop)[0];
+          subscriber.next(headings.map(head => ({
+            ...head,
+            active: head.element === visibleElement
+          })))
+        }
+      }, options);
+
+      headings.forEach(head => {
+        this._intersectionObserver!.observe(head.element!);
+      });
+    })
+  }
+
+  private getTocHeadings(element: HTMLElement) {
     let result: any[] = [];
-    this.contentElement.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(head => {
+    element.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(head => {
       result = [...result, {
         text: head.textContent,
         level: +head.tagName.replace(/h(.)/i, '$1'),
-        element: head,
+        element: this.platformService.isServer ? null : head,
         active: false
       }]
     });
-    this.headings = result;
-
-    const containerElement = this.contentElement.closest('.mat-drawer-content.main-content');
-    let options = {
-      root: containerElement,
-      rootMargin: '0px',
-      threshold: 1.0
-    }
-
-    let visibleElements: HTMLElement[] = [];
-    this.intersectionObserver = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          visibleElements = [...visibleElements, entry.target as HTMLElement];
-        } else {
-          visibleElements = visibleElements.filter(element => element !== entry.target);
-        }
-      });
-
-      if (visibleElements.length > 0) {
-        const visibleElement = visibleElements.sort((a, b) => a.offsetTop - b.offsetTop)[0];
-        this.headings = this.headings.map(head => ({
-          ...head,
-          active: head.element === visibleElement
-        }))
-      }
-    }, options);
-
-    this.headings.forEach(head => {
-      this.intersectionObserver!.observe(head.element);
-    });
+    return result;
   }
 
   ngOnDestroy() {
-    if (this.intersectionObserver) {
-      this.intersectionObserver.disconnect();
+    if (this._intersectionObserver) {
+      this._intersectionObserver.disconnect();
     }
   }
 
-  goTo(target: { text: string; level: number; element: HTMLElement; active: boolean }, event: MouseEvent) {
+  goTo(target: Heading, event: MouseEvent) {
     event.preventDefault();
     const containerElement = findMainContentContainer(this.contentElement);
-    if (containerElement) {
+    if (containerElement && target.element) {
       scrollTo(target.element.offsetTop - 20, containerElement);
     }
   }
