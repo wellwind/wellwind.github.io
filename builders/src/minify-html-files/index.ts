5dabd4f117bb8afc263a4da1727041e5c7e4976c
Mike Huang <wellwind@mail2000.com.tw>
2021-11-07 16:40:21 +0800

    minify html

---
+++ builders/src/minify-html-files/index.ts
@@ -0,0 +1,42 @@
+import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
+import { JsonObject } from '@angular-devkit/core';
+import * as fs from 'fs';
+import * as path from 'path';
+import { minify as minifyHtml } from 'html-minifier';
+
+interface Options extends JsonObject {
+  targetPath: string;
+}
+
+export default createBuilder(minifyHtmlFiles);
+
+async function minifyHtmlFiles(options: Options, context: BuilderContext): Promise<BuilderOutput> {
+  context.logger.info(`📦 Minifying html files in ${options.targetPath}`);
+
+  for (const filePath of getAllHtmlFiles(options.targetPath)) {
+    const result = minifyHtml(
+      fs.readFileSync(filePath).toString('utf-8'),
+      {
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+        collapseInlineTagWhitespace: true,
+        collapseWhitespace: true
+      });
+    fs.writeFileSync(filePath, result);
+  }
+
+  context.logger.info('✅ Done');
+  return { success: true };
+}
+
+function* getAllHtmlFiles(dir: string) {
+  const files = fs.readdirSync(dir, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      yield* getAllHtmlFiles(path.join(dir, file.name));
+    } else if(file.name.endsWith('.html')) {
+      yield path.join(dir, file.name);
+    }
+  }
+}
