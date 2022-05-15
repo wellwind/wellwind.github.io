67434519c04e385a976f225f7e901de3ede0f9f3
Mike <wellwind@mail2000.com.tw>
2022-05-15 17:33:59 +0800

    ✨ feat(theme): add dark theme

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -6,42 +6,42 @@ import { MatDrawerContent } from '@angular/material/sidenav';
 import { NavigationStart, Router } from '@angular/router';
 import { BehaviorSubject, combineLatest, defer } from 'rxjs';
 import { debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs/operators';
-import { slugify } from '../../../utils/slugify';
 import { PlatformService } from '../../platform.service';
-import { PostMetaWithSlug } from '../post-meta.interface';
-import { SiteMetaService } from '../site-meta.service';
 import { SitePostService } from '../site-post.service';
+
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
-  @ViewChild('matDrawerContent') matDrawerContent?: MatDrawerContent
+  @ViewChild('matDrawerContent') matDrawerContent?: MatDrawerContent;
 
   get isServer() {
     return this.platformService.isServer;
   }
+
+  theme$ = new BehaviorSubject<'dark' | 'light'>('dark');
 
   menuOpen$ = new BehaviorSubject<boolean>(true);
   menuItems = [
     { link: '/blog', icon: 'home', text: '首頁' },
     { link: '/blog/categories', icon: 'apps', text: '分類' },
     { link: '/blog/tags', icon: 'label', text: '標籤' },
-    { link: '/blog/archives', icon: 'archive', text: '歸檔' }
+    { link: '/blog/archives', icon: 'archive', text: '歸檔' },
   ];
 
   postCount$ = this.sitePostService.postsMeta$.pipe(
-    map(posts => Object.keys(posts).length)
+    map((posts) => Object.keys(posts).length)
   );
 
   categoryCount$ = this.sitePostService.postCategories$.pipe(
-    map(categories => new Set(categories).size)
+    map((categories) => new Set(categories).size)
   );
 
   tagCount$ = this.sitePostService.postTags$.pipe(
-    map(tags => new Set(tags).size)
+    map((tags) => new Set(tags).size)
   );
 
   isSmallScreen$ = this.platformService.isSmallScreen$;
@@ -53,37 +53,40 @@ export class LayoutComponent implements OnInit {
     this.searchKeyword.valueChanges.pipe(
       debounceTime(300),
       distinctUntilChanged()
-    )])
-    .pipe(
-      switchMap(([posts, keywordString]) =>
-        defer(() => import('../search-posts').then(m => m.searchPosts))
-          .pipe(map(searchFn => searchFn(posts, keywordString)))
+    ),
+  ]).pipe(
+    switchMap(([posts, keywordString]) =>
+      defer(() => import('../search-posts').then((m) => m.searchPosts)).pipe(
+        map((searchFn) => searchFn(posts, keywordString))
       )
     )
+  );
 
   constructor(
     private router: Router,
     private platformService: PlatformService,
     private sitePostService: SitePostService,
-    private matIconRegistry: MatIconRegistry) {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationStart)
-    ).subscribe(url => {
-      if (this.matDrawerContent) {
-        this.matDrawerContent.scrollTo({ top: 0, left: 0 });
-      }
-    });
+    private matIconRegistry: MatIconRegistry
+  ) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe((url) => {
+        if (this.matDrawerContent) {
+          this.matDrawerContent.scrollTo({ top: 0, left: 0 });
+        }
+      });
   }
 
   ngOnInit() {
     this.matIconRegistry.registerFontClassAlias('fontawesome', 'fab');
 
-    this.platformService
-      .isSmallScreen$
-      .pipe(map(result => !result))
-      .subscribe(shouldOpen => {
+    this.platformService.isSmallScreen$
+      .pipe(map((result) => !result))
+      .subscribe((shouldOpen) => {
         this.menuOpen$.next(shouldOpen);
       });
+
+    this.setTheme();
   }
 
   async selectSuggestItem(event: MatAutocompleteSelectedEvent) {
@@ -94,10 +97,36 @@ export class LayoutComponent implements OnInit {
 
   async goSearchPage(autocomplete: MatAutocomplete) {
     if (this.searchKeyword.value) {
-      await this.router.navigate(
-        ['query'],
-        { queryParams: { q: this.searchKeyword.value } });
+      await this.router.navigate(['query'], {
+        queryParams: { q: this.searchKeyword.value },
+      });
       autocomplete._isOpen = false;
     }
+  }
+
+  setTheme() {
+    if (this.platformService.isServer) {
+      return;
+    }
+
+    const themeFromSetting = localStorage.getItem('theme') as
+      | 'dark'
+      | 'light'
+      | null;
+    if (themeFromSetting) {
+      this.theme$.next(themeFromSetting);
+    } else if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: light)').matches
+    ) {
+      this.theme$.next('light');
+    }
+
+    this.theme$.subscribe(theme => {
+      localStorage.setItem('theme', theme);
+      document.body.classList.remove('dark-theme');
+      document.body.classList.remove('light-theme');
+      document.body.classList.add(`${theme}-theme`);
+    })
   }
 }
