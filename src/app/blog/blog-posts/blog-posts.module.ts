93c6c4d8dd281842793c5143e7607797ff64a6fd
Mike Huang <wellwind@mail2000.com.tw>
2023-02-08 21:01:18 +0800

    chore: remove chips

--- src/app/blog/blog-posts/blog-posts.module.ts
+++ src/app/blog/blog-posts/blog-posts.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatRippleModule } from '@angular/material/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -25,7 +25,7 @@ import { BlogPostsComponent } from './blog-posts.component';
     MatButtonModule,
     MatDividerModule,
     MatTooltipModule,
-    MatChipsModule,
+    MatRippleModule,
     PushModule,
   ],
 })
