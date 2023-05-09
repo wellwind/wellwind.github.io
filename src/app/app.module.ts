b32573b0e5187b89c1bc5fac9c2d9bd9f4f97152
Mike Huang <wellwind@mail2000.com.tw>
2023-05-10 00:03:42 +0800

    fix: use APP_ID

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { APP_ID, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,7 +11,6 @@ import { MatListModule } from '@angular/material/list';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UrlSerializer } from '@angular/router';
 import { PushModule } from '@rx-angular/template/push';
@@ -21,29 +20,29 @@ import { LayoutComponent } from './layout/layout.component';
 import { TrailingSlashUrlSerializer } from './trailing-slash-url-serializer';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule.withServerTransition({ appId: 'serverApp' }),
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        MatSidenavModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatAutocompleteModule,
-        MatOptionModule,
-        MatFormFieldModule,
-        MatProgressBarModule,
-        ReactiveFormsModule,
-        MatInputModule,
-        PushModule,
-        LayoutComponent
-    ],
-    providers: [
-        { provide: UrlSerializer, useClass: TrailingSlashUrlSerializer }
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatAutocompleteModule,
+    MatOptionModule,
+    MatFormFieldModule,
+    MatProgressBarModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    PushModule,
+    LayoutComponent,
+  ],
+  providers: [
+    { provide: APP_ID, useValue: 'fullstackladder-app' },
+    { provide: UrlSerializer, useClass: TrailingSlashUrlSerializer },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
