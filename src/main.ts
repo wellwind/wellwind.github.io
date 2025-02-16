e51550cb1f093b707051af4301055d3cea0d386b
Mike Huang <wellwind@mail2000.com.tw>
2025-02-16 08:56:06 +0800

    start o11y

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { startFaro } from './start-faro';
+
+startFaro();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
--- src/start-faro.ts
+++ src/start-faro.ts
@@ -1,19 +1,21 @@
 import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
 import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
-initializeFaro({
-  url: 'https://faro-collector-prod-ap-southeast-1.grafana.net/collect/7589747939196e0549ee6d33aa985355',
-  app: {
-    name: 'Fullstack Ladder',
-    version: '1.0.0',
-    environment: 'production',
-  },
+export const startFaro = () => {
+  initializeFaro({
+    url: 'https://faro-collector-prod-ap-southeast-1.grafana.net/collect/7589747939196e0549ee6d33aa985355',
+    app: {
+      name: 'Fullstack Ladder',
+      version: '1.0.0',
+      environment: 'production',
+    },
 
-  instrumentations: [
-    // Mandatory, omits default instrumentations otherwise.
-    ...getWebInstrumentations(),
+    instrumentations: [
+      // Mandatory, omits default instrumentations otherwise.
+      ...getWebInstrumentations(),
 
-    // Tracing package to get end-to-end visibility for HTTP requests.
-    new TracingInstrumentation(),
-  ],
-});
+      // Tracing package to get end-to-end visibility for HTTP requests.
+      new TracingInstrumentation(),
+    ],
+  });
+};
